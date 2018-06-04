ed5693d1109e03a0f40528af2204d146d7e6f15e
Per Lindström <perlindstroem@gmail.com>
2018-06-04 10:31:44 +0200

    improve header information extraction

--- index.js
+++ index.js
@@ -13,43 +13,49 @@ function parseHtml(html) {
             let containsStats = html.length < 500 && html.length > 100 && html.includes('Antal') && html.includes('Betyg');
 
             if (containsStats) {
-                let date = null;
-                let grades = [];
+                let entry = {
+                    grades: []
+                }
                 let lines = html.split('\n');
                 lines.forEach(line => {
                     let text = $(line).text().split(' ');
 
+                    
                     // should be the header
                     if (text.length > 3) {
-                        date = text.join().slice(-10)
+                        let joined = text.join();
+                        //console.log(text.join())
+                        //console.log(text.join().match(/([A-Z]{4}[0-9]{2})/)[0])
+                        //console.log(text.join().match(/hp([A-Z]{3}[0-9]{1})/)[1])
+                        entry.course = joined.match(/([A-Z]{4}[0-9]{2})/)[0]
+                        entry.type = joined.match(/hp([A-Z]{3}[0-9]{1})/)[1]
+                        entry.date = joined.match(/([0-9]{4}-[0-9]{2}-[0-9]{2})/)[1]
                     }
-
+                    
                     // pair of grade and count
                     if (text.length == 2) {
-                        grades.push({
+                        entry.grades.push({
                             grade: text[0],
                             count: text[1]
                         })
                     }
                 })
 
-                let entry = {
-                    date,
-                    grades,
-                }
+                console.log(JSON.stringify(entry, null, 4));
                 data.push(entry)
                 count = count + 1;
             }
         }
     )
-
+    //console.log(JSON.stringify(data, null, 4));
     return data;
 }
 
-//let course = 'TATA42'
+//let course = 'TSKS06'
 //rp('http://www4.student.liu.se/tentaresult/?kurskod='+course+'&provkod=&datum=&kursnamn=&sort=0&search=S%F6k').then(parseHtml);
 
-const html = fs.readFileSync('./test.html', 'utf-8');
+
+const html = fs.readFileSync('./TSKS06.html', 'utf-8');
 const data = parseHtml(html)
 
 const sum = data.reduce((acc, exam) => {
