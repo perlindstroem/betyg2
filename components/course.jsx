7c5a3fd176e93819ac201bf9959c707cdbb13474
Per Lindström <perlindstroem@gmail.com>
2018-06-19 09:06:12 +0200

    remove lodash

--- components/course.jsx
+++ components/course.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { ResponsiveContainer, BarChart, AreaChart, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import { Button } from 'semantic-ui-react';
-import _ from 'lowdash';
 
 import { parseHtml } from '../services';
 import asd from '../services/TATA41.html';
