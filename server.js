3b1ce1ca972a4168c7c3ba243aa495d3c6df021d
Per Lindström <perlindstroem@gmail.com>
2018-06-24 11:57:34 +0200

    update env variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static('dist'));
 app.listen(port, () => console.log(`access server on http://localhost:${port}`));
