59e758ed2f0ccaa9abcfb86a7b31bf17e30ca95b
Per Lindström <perlindstroem@gmail.com>
2018-06-24 11:48:47 +0200

    update webpack conf and build

---
+++ config/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const outputPath = path.join(__dirname, '../dist');
+const templatePath = path.join(__dirname, '../components/index.html');
+
+module.exports = (env, argv) => ({
+  entry: ['babel-polyfill', './components/index.jsx'],
+  mode: env.production ? 'production' : 'development',
+  output: {
+    path: outputPath,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.jsx$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.html$/, exclude: /node_modules/, use: 'html-loader' },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: ['url-loader'] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: templatePath }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: outputPath,
+    hot: true,
+  },
+  devtool: env.production ? '' : 'inline-source-map',
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+});
