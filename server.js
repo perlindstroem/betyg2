a770d2bdaa2b8a665a3e0d5010eafc3018e4861e
Per Lindström <perlindstroem@gmail.com>
2018-08-22 20:38:29 +0200

    Add redirect

--- server.js
+++ server.js
@@ -5,5 +5,17 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static('dist'));
 
+app.use('*', (req, res, next) => {
+  if (req.secure) {
+      console.log('secure')
+    // request was via https, so do no special handling
+    next();
+  } else {
+      console.log('not secure', `https://${req.headers.host}${req.url}`)
+    // request was via http, so redirect to https
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 app.use('/api', apiRouter);
 app.listen(port, () => console.log(`access server on http://localhost:${port}`));
