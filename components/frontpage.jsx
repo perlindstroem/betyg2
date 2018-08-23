ce5190949329b805405547b1c346469a64d4ea7b
Per Lindström <perlindstroem@gmail.com>
2018-08-23 14:47:41 +0200

    Add analytics and fix spelling

--- components/frontpage.jsx
+++ components/frontpage.jsx
@@ -12,7 +12,7 @@ export default class Frontpage extends Component {
       <Segment style={{ width: '100%', textAlign: 'left', fontSize: '1.2em' }} loading={this.props.loading}>
         <Header as="h2"><Icon name="hand spock" />Kul att du hittat hit!</Header>
         <p>Här kan du hitta visualisering av kursstatistik från Linköpings Universitet. För att komma igång; skriv in en kurskod i sökfältet och.. äh, du förstår nog hur man ska göra, det är inte jättesvårt. Kör bara kör!</p>
-        <p>Sidans funktionalitet är kanske något begränsad, fler features kommer när det tid, ork och motivation finnes. För önskemål, skicka mail!</p>
+        <p>Sidans funktionalitet är kanske något begränsad, fler features kommer när tid, ork och motivation finnes. För önskemål, skicka mail!</p>
         <p>All inspiration för sidan kommer från <em>betygen.se</em>. RIP.</p>
         <Header as="h2"><Icon name="mail" />Kontakt</Header>
         <p>Hittat en bug? Vill du diskutera betygsfördelningen på flervariabeln över en kaffe?</p>
